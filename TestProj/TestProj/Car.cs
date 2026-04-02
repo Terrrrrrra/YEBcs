2e2668af6f863edfa645f5606dc92c1b8c3cdd1e
jks <jksjks8357@gmail.com>
2026-04-02 20:52:43 +0900

    클래스 생성자

--- TestProj/TestProj/Car.cs
+++ TestProj/TestProj/Car.cs
@@ -14,12 +14,22 @@ namespace TestProj
         public double Cost;
         public string Name;
 
+        // 함수구조 : 접근제한자 + 반환타입 + 함수이름
         public void PrintInfo()
         {
             Console.WriteLine($"타이어 개수 : {Tire}");
             Console.WriteLine($"속도 : {Speed}");
             Console.WriteLine($"가격 : {Cost}");
             Console.WriteLine($"이름 : {Name}");
+        }
+
+        // 생성자구조 : 접근제한자 + 클래스이름
+        public Car(int tire, float speed, double cost, string name)
+        {
+            Tire = tire;
+            Speed = speed;
+            Cost = cost;
+            Name = name;
         }
     }
 }
--- TestProj/TestProj/Program.cs
+++ TestProj/TestProj/Program.cs
@@ -10,30 +10,19 @@ namespace TestProj
             // 클래스 == 설계도
             // 객체 == 설계도를 통해 실제 존재하는것을 만든것
 
-            //List<int> intList = new List<int>();
-            Cat();
-
             // Car 는 클래스
             // 클래스 뒤에 ()는 생성자
             // new Car() 는 Car 타입 객체를 새로 생성한다는 의미
 
-            Car mycar = new Car();
-
-            // double = 8바이트, float = 4바이트
-            // double -> float 바로 전환 안됨, 숫자뒤에 f 붙여주기
+            Car mycar = new Car(6, 32.4f, 2000_0000, "현대자동차");
 
             mycar.PrintInfo();
 
-            mycar.Tire = 4;
+            /*mycar.Tire = 4;
             mycar.Speed = 50.3f;
             mycar.Cost = 10_000_000.5;
-            mycar.Name = "기아자동차";
+            mycar.Name = "기아자동차";*/
 
-            mycar.PrintInfo();
-            //print(mycar.Tire);
-            //print(mycar.Speed);
-            //print(mycar.Cost);
-            //print(mycar.Name);
         }
 
         static void Cat()
