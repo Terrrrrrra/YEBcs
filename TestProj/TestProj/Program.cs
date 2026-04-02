b0855d52ca6a98206ba1137d446480e3d88e4a08
jks <jksjks8357@gmail.com>
2026-04-02 19:03:04 +0900

    구구단 만들기

--- TestProj/TestProj/Program.cs
+++ TestProj/TestProj/Program.cs
@@ -7,19 +7,35 @@ namespace TestProj
     {
         static void Main(string[] args)
         {
-            int num1 = 12;
+            Console.WriteLine("===구구단을 외자===");
+            Console.Write("구구단 몇단? : ");
+            int input = int.Parse(Console.ReadLine());
 
-            Console.WriteLine($"저는 {num1}살 입니다.");
-
-
-            for (int i = 0; i < 9; i++)
+            /*
+            구구단 몇단? : 2
+            2 * 1 = 2
+            2 * 2 = 4
+            2 * 3 = 6
+            2 * 4 = 8
+            2 * 5 = 10
+            2 * 6 = 12
+            2 * 7 = 14
+            2 * 8 = 16
+            2 * 9 = 18
+            */
+            // 선언식, 조건식, 증감식
+            // 선언식에선? int i = 1
+            // 조건식에선? i < 10, i <= 9
+            // 증감식에선? i++
+            for(int i = 1; i < 10; i++)
             {
-                Console.WriteLine($"그로부터 {i}년이 지났습니다..");
+                // 코드부분에선?
+                Console.WriteLine($"{input} * {i} = {input*i}");
             }
 
-            Console.Write("숫자를 입력하세요 : ");
-            int inputNum = int.Parse(Console.ReadLine());
-            Console.WriteLine($"입력받은 숫자는 {inputNum}입니다.");
+
+
+            Console.WriteLine("프로그램 종료");
         }
     }
 }
