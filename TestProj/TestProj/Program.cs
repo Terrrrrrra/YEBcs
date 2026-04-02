4bab497322d5e9f0c1bd308748cef8b8554f8f74
jks <jksjks8357@gmail.com>
2026-04-02 20:00:50 +0900

    func basic 1 | 함수 기초 배우기 1

--- TestProj/TestProj/Program.cs
+++ TestProj/TestProj/Program.cs
@@ -7,35 +7,86 @@ namespace TestProj
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("===구구단을 외자===");
-            Console.Write("구구단 몇단? : ");
-            int input = int.Parse(Console.ReadLine());
+            int num1 = Calc1(7);
+            print(num1);
 
-            /*
-            구구단 몇단? : 2
-            2 * 1 = 2
-            2 * 2 = 4
-            2 * 3 = 6
-            2 * 4 = 8
-            2 * 5 = 10
-            2 * 6 = 12
-            2 * 7 = 14
-            2 * 8 = 16
-            2 * 9 = 18
-            */
-            // 선언식, 조건식, 증감식
-            // 선언식에선? int i = 1
-            // 조건식에선? i < 10, i <= 9
-            // 증감식에선? i++
-            for(int i = 1; i < 10; i++)
-            {
-                // 코드부분에선?
-                Console.WriteLine($"{input} * {i} = {input*i}");
-            }
+            int num2 = Calc1(4);
+            print(num2);
+
+            int num3 = Calc2(24, 13);
+            print(num3);
+        }
+
+        static int Calc2(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Calc1(int a)
+        {
+            int b = a + a;
+            return b;
+        }
+
+        static int GetNumber()
+        {
+            int a = 12;
+            int b = 25;
+            int c = a + b;
+            return c;
+        }
+
+
+        static void Dog()
+        {
+            print("강아지는....");
+            return;
+            print("멍멍!");
+        }
+
+
+        static void Cat()
+        {
+            print("고양이는 야옹야옹");
+        }
 
 
 
-            Console.WriteLine("프로그램 종료");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        static void print(object obj)
+        {
+            Console.WriteLine(obj.ToString());
         }
     }
 }
