fed51547d7570b6c18651b99d191f51e954edbfd
jks <jksjks8357@gmail.com>
2026-04-09 17:35:40 +0900

    클래스 객체 생성 연습

---
+++ TestProj/TestProj/Pokemon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProj
+{
+    // 이름, 체력, 스피드, 기술
+    internal class Pokemon
+    {
+        // 클래스 내에 존재하는 변수들 == 필드
+        public string Name;
+        public int Hp;
+        public float Speed;
+
+        public void Skill()
+        {
+            Console.WriteLine($"{Name}(이)가 몸통박치기를 시전했다...!");
+            Console.WriteLine($"현재체력 : {Name} | 현재 스피드 : {Speed}");
+        }
+
+        public Pokemon(string name, int hp, float speed)
+        {
+            Name = name;
+            Hp = hp;
+            Speed = speed;
+        }
+    }
+}
--- TestProj/TestProj/Program.cs
+++ TestProj/TestProj/Program.cs
@@ -7,27 +7,22 @@ namespace TestProj
     {
         static void Main(string[] args)
         {
-            // 클래스 == 설계도
-            // 객체 == 설계도를 통해 실제 존재하는것을 만든것
 
-            // Car 는 클래스
-            // 클래스 뒤에 ()는 생성자
-            // new Car() 는 Car 타입 객체를 새로 생성한다는 의미
+            Pokemon pokemon1 = new Pokemon("파이리", 100, 200);
+            Pokemon pokemon2 = new Pokemon("피카츄", 80, 250.5f);
+            Pokemon pokemon3 = new Pokemon("꼬부기", 150, 145.8f);
+            //curPokemon.Name = "파이리";
+            //curPokemon.Hp = 100;
 
-            Car mycar = new Car(6, 32.4f, 2000_0000, "현대자동차");
+            List<Pokemon> pokemonList = new List<Pokemon>();
+            pokemonList.Add(pokemon1);
+            pokemonList.Add(pokemon2);
+            pokemonList.Add(pokemon3);
 
-            mycar.PrintInfo();
-
-            /*mycar.Tire = 4;
-            mycar.Speed = 50.3f;
-            mycar.Cost = 10_000_000.5;
-            mycar.Name = "기아자동차";*/
-
-        }
-
-        static void Cat()
-        {
-            print("고양이는 야옹야옹");
+            for (int i = 0; i < pokemonList.Count; i++)
+            {
+                pokemonList[i].Skill();
+            }
         }
 
         static void print(object obj)
