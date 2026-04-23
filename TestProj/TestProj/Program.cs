ec225d8df70b1c1d15df8f911ef01af7bd75f072
jks <jksjks8357@gmail.com>
2026-04-23 19:22:30 +0900

    에러 처리 2

--- TestProj/TestProj/Program.cs
+++ TestProj/TestProj/Program.cs
@@ -6,54 +6,32 @@ namespace TestProj
     {
         static void Main(string[] args)
         {
-            int[] intArr = { 0, 1, 2, 3 };
-            Console.Write("정수 입력 : ");
-            string input = Console.ReadLine();
-            int inputNum = -1;
-            int curNum = -1;
-
             try
             {
-                inputNum = int.Parse(input);
-                curNum = intArr[4];
-            }
-            catch (FormatException ex)
-            {
-                print("정수로 입력하세요!");
-            }
-            catch (IndexOutOfRangeException ex)
-            {
-                print("인덱스 범위 벗어남!");
-            }
-            catch (Exception ex)
-            {
-                print(ex.Message);
-            }
-
-            print(inputNum);
-            print(curNum);
-
-
-            Console.Write("정수 입력 : ");
-            string input2 = Console.ReadLine();
-            TryParse(input2, out int inputNum2);
-            print($"입력한 정수 : {inputNum2}");
-        }
-
-        static void TryParse(string s, out int num)
-        {
-            try
-            {
-                num = int.Parse(s);
-                print("변환 성공!");
+                Error2();
             }
             catch
             {
-                num = 0;
-                print("변환 실패...");
+                print("에러발생, Main에서 처리");
             }
         }
 
+        static void Error1()
+        {
+            throw new Exception();
+        }
+
+        static void Error2()
+        {
+            try
+            {
+                Error1();
+            }
+            catch
+            {
+                print("에러발생, Error2에서 처리");
+            }
+        }
 
         static void print(object obj)
         {
